f31d1aa6e7785845d71621f2e356eb2abefeb934
lorenzofoschi <91487500+ihcsof@users.noreply.github.com>
2023-08-08 05:34:58 +0200

    Delete ADC directory

--- ADC/hello012/ken_header.h
+++
@@ -1,6 +0,0 @@
-#define INITIAL_PRINTF \
-	printf("/*** Copyright (C) Giovanni Chiola <chiolag@acm.org> 2010-2021\n"); \
-	printf(" *** All rights reserved.\n ***/\n\n"); \
-	printf("#include \"ken_header.h\"\n\n"); \
-	printf("#include<stdio.h>\n#include<stdlib.h>\nchar\ts[]={\n");
-
